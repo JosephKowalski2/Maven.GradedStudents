7b2a56faba13c8617fec6cbe774a13591bc446a9
Joseph Kowalski <jkjr25@gmail.com>
2022-03-08 16:39:23 -0500

    Added all student

--- src/main/java/io/zipcoder/Student.java
+++ src/main/java/io/zipcoder/Student.java
@@ -31,16 +31,39 @@ public class Student {
     }
 
     public Integer getNumberOfExamsTaken(){
-        return getExamScores().size();
+        return examScores.size();
     }
 
     public String getExamScores(){
         String scores = "";
         for (int i = 0; i < getNumberOfExamsTaken(); i++){
-            scores += "Exam " + i + "score is " + examScores.get(i) + "/n";
+            scores += "Exam " + (i + 1)  + "score is " + examScores.get(i) + "/n";
         }
         return scores;
     }
 
+    public void addExamScore(double examScore){
+        examScores.add(examScore);
+    }
 
+    public void setExamScore(int examNumber, double newScore){
+//        examScores.remove(examNumber - 1);
+//        examScores.add(examNumber - 1, newScore);
+        examScores.set(examNumber - 1, newScore);
+    }
+
+    public Double getAverageExamScore(){
+        Double average = 0.0;
+        for (int i = 0; i < examScores.size(); i++){
+            average += examScores.get(i);
+        }
+        return average / examScores.size();
+    }
+
+    public String toString(){
+        String studentInfo = firstName + " " + lastName + "/n";
+        studentInfo += "Average Score: " + getAverageExamScore() + "/n";
+        studentInfo += getExamScores();
+        return studentInfo;
+    }
 }
