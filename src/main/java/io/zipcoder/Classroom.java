ea59beac927eb9f36bcdba626652b8f331ccd470
Joseph Kowalski <jkjr25@gmail.com>
2022-03-08 17:37:36 -0500

    Added stuff to classroom class

--- src/main/java/io/zipcoder/Classroom.java
+++ src/main/java/io/zipcoder/Classroom.java
@@ -1,4 +1,42 @@
 package io.zipcoder;
 
+import java.util.Arrays;
+
 public class Classroom {
+    private Student[] students;
+
+    public Classroom(int maxNumberOfStudents){
+        students = new Student[maxNumberOfStudents];
+    }
+
+    public Classroom(Student[] student){
+        students = student;
+    }
+
+    public Classroom(){
+        this(30);
+    }
+
+    public Student[] getStudents() {
+        return students;
+    }
+
+    public Double getAverageExamScore(){
+        Double average = 0.0;
+        for (int i = 0; i < students.length; i++){
+            average += students[i].getAverageExamScore();
+        }
+        return average / students.length;
+    }
+
+    public void addStudent(Student student){
+//        for (int i = 0; i < students.length; i++){
+//            students[i] = student;
+//        }
+        Arrays.fill(students, student);
+    }
+
+    public void removeStudent(String firstName, String lastName){
+
+    }
 }
