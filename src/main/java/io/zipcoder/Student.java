ecc340e1f0b5fcfbaad0286320172f6c50efe9cf
Joseph Kowalski <jkjr25@gmail.com>
2022-03-08 16:14:34 -0500

    Added some of student

--- src/main/java/io/zipcoder/Student.java
+++ src/main/java/io/zipcoder/Student.java
@@ -1,4 +1,46 @@
 package io.zipcoder;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Student {
+    private String firstName;
+    private String lastName;
+    private ArrayList<Double> examScores;
+
+    public Student(String firstName, String lastName, Double[] examScores) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.examScores = new ArrayList<>(Arrays.asList(examScores));
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setFirstName(String firstName){
+        this.firstName = firstName;
+    }
+
+    public void setLastName (String lastName){
+        this.lastName = lastName;
+    }
+
+    public Integer getNumberOfExamsTaken(){
+        return getExamScores().size();
+    }
+
+    public String getExamScores(){
+        String scores = "";
+        for (int i = 0; i < getNumberOfExamsTaken(); i++){
+            scores += "Exam " + i + "score is " + examScores.get(i) + "/n";
+        }
+        return scores;
+    }
+
+
 }
